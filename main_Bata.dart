80240d6f6f37833b897495daef08f608101daed9
NosTre <nostre20@gmail.com>
2020-04-04 15:51:14 +0200

    first checkin fix

--- main_Bata.dart
+++ main_Bata.dart
@@ -1,207 +1,390 @@
-import 'dart:async';
 import 'dart:io';
-
-import 'package:camera/camera.dart';
-import 'package:flutter/material.dart';
-import 'package:path/path.dart' show join;
+import 'package:flutter/material.dart'; //it has similar properties as widgets.dart (pw)
+import 'package:open_file/open_file.dart';
+import 'package:pdf/pdf.dart';
+import 'package:pdf/widgets.dart' as pw;
 import 'package:path_provider/path_provider.dart';
 
-Future<void> main() async {
-  // Ensure that plugin services are initialized so that `availableCameras()`
-  // can be called before `runApp()`
-  WidgetsFlutterBinding.ensureInitialized();
-
-  // Obtain a list of the available cameras on the device.
-  final cameras = await availableCameras();
-
-  // Get a specific camera from the list of available cameras.
-//  final lastCamera = cameras.last;
-  var firstCamera = cameras[0];
-  for (var i=0; i<cameras.length; i++) {
-    if(cameras[i].lensDirection == CameraLensDirection.front) {
-      firstCamera = cameras[i];
-    }
-  }
-
-  runApp(
-    MaterialApp(
-      theme: ThemeData.dark(),
-      home: TakePictureScreen(
-        // Pass the appropriate camera to the TakePictureScreen widget.
-        camera: firstCamera,
-      ),
-    ),
-  );
+void main() {
+  runApp(App());
 }
 
-// A screen that allows users to take a picture using a given camera.
-class TakePictureScreen extends StatefulWidget {
-  final CameraDescription camera;
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(title: "Számonkérési alkalmazás"),
+    );
+  }
+}
 
-  const TakePictureScreen({
-    Key key,
-    @required this.camera,
-  }) : super(key: key);
+class HomePage extends StatelessWidget {
+  final String title;
+
+  HomePage({Key key, this.title}) : super(key: key);
 
   @override
-  TakePictureScreenState createState() => TakePictureScreenState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar( title: Text(title)),
+      body: Center(
+        child: RaisedButton(
+          child: Text("Teszt indítása"),
+          onPressed: () async {
+            List<PersonEntry> persons = await Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => SOF(),
+              ),
+            );
+            if (persons != null) persons.forEach(print);
+          },
+        ),
+      ),
+    );
+  }
 }
 
-class TakePictureScreenState extends State<TakePictureScreen> {
-  String path;
-  String videoPath;
-  bool _isRecording = false;
-  CameraController _controller;
-  Future<void> _initializeControllerFuture;
+class SOF extends StatefulWidget {
+  @override
+  _SOFState createState() => _SOFState();
+}
+
+class _SOFState extends State<SOF> {
+  var nameTECs = <TextEditingController>[];
+  var ageTECs = <TextEditingController>[];
+  var jobTECs = <TextEditingController>[];
+  var cards = <Card>[];
+
+  final TextEditingController megoldas1TextController = TextEditingController();
+
+  PdfHandler m_PdfHandler = PdfHandler("reportPdf.pdf");
+
+  String get feladat1 => "Ez egy feladat. Ez egy feladat. Ez egy nagyon hosszu feladat. Ez egy feladat. Ez egy feladat. Ez egy nagyon hosszu feladat. ";
+
+  TextField get textMultiLine => TextField(
+    textInputAction: TextInputAction.newline,
+    keyboardType: TextInputType.multiline,
+    maxLines: null,
+
+  );
+
+  Card createCard() {
+    var nameController = TextEditingController();
+    var ageController = TextEditingController();
+    var jobController = TextEditingController();
+    nameTECs.add(nameController);
+    ageTECs.add(ageController);
+    jobTECs.add(jobController);
+    return Card(
+      child: Column(
+
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Text('Person ${cards.length + 1}'),
+          TextField(
+              controller: nameController,
+              decoration: InputDecoration(labelText: 'Full Name')),
+          TextField(
+              controller: ageController,
+              decoration: InputDecoration(labelText: 'Age')),
+          TextField(
+              controller: jobController,
+              decoration: InputDecoration(labelText: 'Study/ job')),
+          Container(
+            margin: EdgeInsets.all(12), //a képernyő széleitől 12 pixellel béjebb kezdődik a textfield minden irányban
+            height: 3 * 24.0, //hány sor hosszú legyen * 24
+            child: Text(
+              feladat1,
+              overflow: TextOverflow.ellipsis,
+              maxLines: 3,
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.all(12), //a képernyő széleitől 12 pixellel béjebb kezdődik a textfield minden irányban
+            height: 10 * 24.0, //hány sor hosszú legyen * 24
+            child: TextField(
+              maxLines: 10,
+              controller: megoldas1TextController,
+              decoration: InputDecoration(
+                hintText: "Írd ide a megoldást",
+                fillColor: Colors.grey[300],
+                filled: true,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 
   @override
   void initState() {
     super.initState();
-    // To display the current output from the Camera,
-    // create a CameraController.
-    _controller = CameraController(
-      // Get a specific camera from the list of available cameras.
-      widget.camera,
-      // Define the resolution to use.
-      ResolutionPreset.medium,
-    );
-
-    // Next, initialize the controller. This returns a Future.
-    _initializeControllerFuture = _controller.initialize();
+    cards.add(createCard());
   }
 
-  @override
-  void dispose() {
-    // Dispose of the controller when the widget is disposed.
-    _controller.dispose();
-    super.dispose();
+  _onDone() {
+    List<PersonEntry> entries = [];
+    for (int i = 0; i < cards.length; i++) {
+      var name = nameTECs[i].text;
+      var age = ageTECs[i].text;
+      var job = jobTECs[i].text;
+      entries.add(PersonEntry(name, age, job));
+    }
+    Navigator.pop(context, entries);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Take a picture')),
-      // Wait until the controller is initialized before displaying the
-      // camera preview. Use a FutureBuilder to display a loading spinner
-      // until the controller has finished initializing.
-      body: FutureBuilder<void>(
-        future: _initializeControllerFuture,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.done) {
-            // If the Future is complete, display the preview.
-            return Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                RaisedButton(
-                  child: Icon(Icons.camera_alt),
-                  // Provide an onPressed callback.
-                  onPressed: () async {
-                    // Take the Picture in a try / catch block. If anything goes wrong,
-                    // catch the error.
-                    try {
-                      // Ensure that the camera is initialized.
-                      await _initializeControllerFuture;
+      appBar: AppBar(),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: ListView.builder(
+              itemCount: cards.length,
+              itemBuilder: (BuildContext context, int index) {
+                return cards[index];
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: RaisedButton(
+              child: Text('Előre'),
+              onPressed: () async {
+                List<PersonEntry> persons = await Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => SOF(),
+                  ),
+                );
+                if (persons != null) persons.forEach(print);
+              },
+            ),
+          ),
+          RaisedButton(
+            child: Text("Vissza"),
+              onPressed: () {
+                Navigator.pop(context);
+              }
+          ),
+          RaisedButton(
+            child: Text("AddPdfPage"),
+            onPressed: () {
+              m_PdfHandler.AddFeladatPage();
+            },
+          ),
+          RaisedButton(
+            child: Text("AddImagePage"),
+            onPressed: () {
+              m_PdfHandler.AddImagePage();
+            },
+          ),
+          RaisedButton(
+            child: Text("SavePdf"),
+            onPressed: () {
+              m_PdfHandler.SavePdf();
+            },
+          ),
+          RaisedButton(
+            child: Text("OpenPdf"),
+            onPressed: () {
+              m_PdfHandler.OpenPdf();
+            },
+          ),
 
-                      // Construct the path where the image should be saved using the
-                      // pattern package.
-                      path = join(
-                        // Store the picture in the temp directory.
-                        // Find the temp directory using the `path_provider` plugin.
-                        (await getTemporaryDirectory()).path,
-                        '${DateTime.now()}.png',
-                      );
-
-                      // Attempt to take a picture and log where it's been saved.
-                      await _controller.takePicture(path);
-
-                    } catch (e) {
-                      // If an error occurs, log the error to the console.
-                      print(e);
-                    }
-                  },
-                ),
-                RaisedButton(
-                  child: Icon(Icons.panorama),
-                  // Provide an onPressed callback.
-                  onPressed: () async {
-                    // Take the Picture in a try / catch block. If anything goes wrong,
-                    // catch the error.
-                    try {
-                      // Ensure that the camera is initialized.
-                      await _initializeControllerFuture;
-
-                      // If the picture was taken, display it on a new screen.
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => DisplayPictureScreen(imagePath: path),
-                        ),
-                      );
-                    } catch (e) {
-                      // If an error occurs, log the error to the console.
-                      print(e);
-                    }
-                  },
-                ),
-                RaisedButton(
-                  child: Icon(Icons.videocam),
-                  // Provide an onPressed callback.
-                  onPressed: () async {
-                    // Take the Picture in a try / catch block. If anything goes wrong,
-                    // catch the error.
-                    try {
-                      // Ensure that the camera is initialized.
-                      await _initializeControllerFuture;
-
-                      // Construct the path where the image should be saved using the
-                      // pattern package.
-                      videoPath = join(
-                        // Store the picture in the temp directory.
-                        // Find the temp directory using the `path_provider` plugin.
-                        (await getExternalStorageDirectory()).path,
-                        '${DateTime.now()}.mp4',
-                      );
-
-                      // Attempt to take a picture and log where it's been saved.
-                      if (!_isRecording) {
-                        await _controller.startVideoRecording(videoPath);
-                        _isRecording = true;
-                      } else {
-                        await _controller.stopVideoRecording();
-                        _isRecording = false;
-                      }
-
-                    } catch (e) {
-                      // If an error occurs, log the error to the console.
-                      print(e);
-                    }
-                  },
-                ),
-              ],
-            );
-          } else {
-            // Otherwise, display a loading indicator.
-            return Center(child: CircularProgressIndicator());
-          }
-        },
+        ],
       ),
+      floatingActionButton:
+      FloatingActionButton(child: Icon(Icons.done), onPressed: _onDone),
     );
   }
 }
 
-// A widget that displays the picture taken by the user.
-class DisplayPictureScreen extends StatelessWidget {
-  final String imagePath;
+class PersonEntry {
+  final String name;
+  final String age;
+  final String studyJob;
 
-  const DisplayPictureScreen({Key key, this.imagePath}) : super(key: key);
-
+  PersonEntry(this.name, this.age, this.studyJob);
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Display the Picture')),
-      // The image is stored as a file on the device. Use the `Image.file`
-      // constructor with the given path to display the image.
-      body: Image.file(File(imagePath)),
-    );
+  String toString() {
+    return 'Person: name= $name, age= $age, study job= $studyJob';
+  }
+}
+
+
+class PdfHandler {
+  final String s_MainHeader = "Írásbeli számonkérés";
+  final String s_Feladat = "Feladat";
+  final String s_Leiras = "Leiras";
+  final String s_Megoldas = "Megoldas";
+  final String s_Kepek = "Kepek";
+  final String s_Metrika = "Metrika";
+
+  int feladatCount = 0;
+
+  String m_FileName;
+  pw.Document m_PdfDoc;
+
+  PdfHandler(String _fileName) {
+    m_FileName = _fileName;
+    CreateDoc();
+  }
+
+  void CreateDoc() {
+    m_PdfDoc = pw.Document();
+  }
+
+  void AddFeladatPage() {
+    feladatCount++;
+    m_PdfDoc.addPage(pw.MultiPage(
+        pageFormat: GetPageFormat(),
+        crossAxisAlignment: getCrossAxisAlignment(),
+        header: (pw.Context context) {
+          return GetHeader(context);
+        },
+        footer: (pw.Context context) {
+          return getFooter(context);
+        },
+        build: (pw.Context context) =>
+        <pw.Widget>[
+          pw.Header(
+              level: 0,
+              child: pw.Row(
+                  mainAxisAlignment: pw.MainAxisAlignment.spaceBetween,
+                  children: <pw.Widget>[
+                    pw.Text(s_MainHeader, textScaleFactor: 2)
+                  ])),
+          pw.Header(level: 1, text: "${feladatCount}. ${s_Feladat}"),
+          pw.Header(level: 2, text: s_Leiras),
+          pw.Paragraph(
+              text: "feladat"
+          ),
+          pw.Header(level: 2, text: s_Megoldas),
+          pw.Paragraph(
+              text: "megoldas"
+          ),
+        ]));
+  }
+
+  void AddImagesPage() {
+    m_PdfDoc.addPage(pw.MultiPage(
+        pageFormat: GetPageFormat(),
+        crossAxisAlignment: getCrossAxisAlignment(),
+        header: (pw.Context context) {
+        return GetHeader(context);
+        },
+        footer: (pw.Context context) {
+        return getFooter(context);
+        },
+        build: (pw.Context context) =>
+        <pw.Widget>[
+          pw.Header(
+              level: 0,
+              child: pw.Row(
+                  mainAxisAlignment: pw.MainAxisAlignment.spaceBetween,
+                  children: <pw.Widget>[
+                    pw.Text(s_MainHeader, textScaleFactor: 2)
+                  ])),
+          pw.Header(level: 1, text: s_Kepek),
+
+
+/*          pw.Table.fromTextArray(context: context, data: const <List<pw.Image>>[
+            <pw.Image>[PdfImage.file(m_PdfDoc.document, bytes: File('test.webp').readAsBytesSync())],
+          ]),*/
+        ]));
+  }
+
+  void AddImagePage() async{
+   // Image.asset('assets/images/lake.jpg')
+   // final image = PdfImage.file(m_PdfDoc.document, bytes: File('assets/images/evosoft-logo.jpg').readAsBytesSync());
+
+//    PdfImage logoImage = await pdfImageFromImageProvider(
+//      pdf: m_PdfDoc.document,
+//      image: AssetImage('assets/images/evosoft-logo.jpg'),
+//    );
+//
+//    m_PdfDoc.addPage(pw.Page(
+//        build: (pw.Context context) {
+//          return pw.Center(
+//            child: pw.Image(logoImage),
+//          ); // Center
+//        })); // Page
+
+    var systemTempDir = (await getTemporaryDirectory()).path;
+    var file = Directory("$systemTempDir").listSync();
+    // List directory contents, recursing into sub-directories,
+    // but not following symbolic links.
+    int i = 0;
+    for(i = 0; i < file.length; i++)
+    {
+      if(file[i].statSync() == FileSystemEntityType.file)
+      print(file[i].toString());
+    }
+//
+//    final output = await getDownloadsDirectory();
+//    print('${output.path}/${m_FileName}');
+//    OpenFile.open('${output.path}/${m_FileName}', type: "image/jpeg", uti:  "public.jpeg");
+//
+
+  }
+
+  void SavePdf() async {
+    final output = await getTemporaryDirectory();
+    final file = File("${output.path}/${m_FileName}");
+    print("${output.path}/${m_FileName}");
+    // final File file = File('example.pdf');
+    file.writeAsBytesSync(m_PdfDoc.save());
+  }
+
+  void OpenPdf() async {
+    final output = await getTemporaryDirectory();
+    OpenFile.open('${output.path}/${m_FileName}', type: "application/pdf", uti:  "com.adobe.pdf");
+    print('opened');
+    print('${output.path}/${m_FileName}');
+  }
+
+  PdfPageFormat GetPageFormat() {
+    return PdfPageFormat.letter.copyWith(marginBottom: 1.5 * PdfPageFormat.cm);
+  }
+
+  pw.Container GetHeader(pw.Context context) {
+      if (context.pageNumber == 1) {
+        return null;
+      }
+      return pw.Container(
+          alignment: pw.Alignment.centerRight,
+          margin: const pw.EdgeInsets.only(bottom: 3.0 * PdfPageFormat.mm),
+          padding: const pw.EdgeInsets.only(bottom: 3.0 * PdfPageFormat.mm),
+          decoration: const pw.BoxDecoration(
+              border: pw.BoxBorder(
+                  bottom: true, width: 0.5, color: PdfColors.grey)),
+          child: pw.Text(s_MainHeader,
+              style: pw.Theme
+                  .of(context)
+                  .defaultTextStyle
+                  .copyWith(color: PdfColors.grey)));
+  }
+
+  pw.CrossAxisAlignment getCrossAxisAlignment() {
+    return pw.CrossAxisAlignment.start;
+  }
+
+  pw.Container getFooter(pw.Context context) {
+    return pw.Container(
+        alignment: pw.Alignment.centerRight,
+        margin: const pw.EdgeInsets.only(top: 1.0 * PdfPageFormat.cm),
+        child: pw.Text(
+            'Page ${context.pageNumber} of ${context.pagesCount}',
+            style: pw.Theme
+                .of(context)
+                .defaultTextStyle
+                .copyWith(color: PdfColors.grey)));
   }
 }
