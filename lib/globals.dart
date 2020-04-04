d7b3ab6c2a4e2915cc0310d1836719349741d3ad
NosTre <nostre20@gmail.com>
2020-04-04 21:10:26 +0200

    missing part

---
+++ lib/globals.dart
@@ -0,0 +1,14 @@
+library my_prj.globals;
+
+class FeladatMegoldasParos {
+  String feladat = "";
+  String megoldas = "";
+
+  FeladatMegoldasParos(this.feladat);
+}
+
+final List<FeladatMegoldasParos> feladatmegoldasLista = [];
+
+int feladatSorszam = 0;
+
+bool beadva = false;
